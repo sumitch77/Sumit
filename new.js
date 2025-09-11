1a6d2dd80cc2f6620750a21c83005e5f9dc99157
sumitch77 <sumitchaudhary282006@gmail.com>
2025-09-11 14:50:23 +0530

    Index1 and new.js and index updated

---
+++ new.js
@@ -0,0 +1,15 @@
+console.log("hello");
+let new = prompt("enter password");
+
+if(new==="7728")
+{
+    window.location.href="index.html";
+}
+else
+{
+    console.log("login denied");
+    window.location.href="index1.html";
+}
+
+
+
