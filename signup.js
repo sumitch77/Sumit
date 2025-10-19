a6aec23c487810db7e9dbe7d1300989150665291
Tourism <sumitchaudhary282006@gmail.com>
2025-10-19 12:10:05 +0530

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,16 +1,16 @@
 let create = document.querySelector("#login1");
 let name1 = document.querySelector("#username");
 let pass1 = document.querySelector("#password");
-let name1="";
-let pass1="";
+let wrong= document.querySelector("#wrong");
+
 create.addEventListener("click", (e) =>{
     e.preventDefault();
-    if(name1==="" && pass1===""){
-        
+    if(name1.value==="" ||  pass1.value===""){
+      wrong.style.display="block";
+      return;
     }
-    else{
   localStorage.setItem("username", name1.value);
 localStorage.setItem("password", pass1.value);
 window.location.href="indexm.html";  
-    }
+    
 });
