1dfda20438a7dfd410d29c1236d4883ce4dc4e40
Tourism <sumitchaudhary282006@gmail.com>
2026-02-08 20:58:43 +0530

    Add files via upload

--- wiki.js
+++ wiki.js
@@ -2,7 +2,12 @@ let ques = document.querySelector("#ques");
 let but = document.querySelector("#get");        
 let answer = document.querySelector("#answer");        
 let answer2 = document.querySelector("#answer2");        
+let but2 = document.querySelector("#get2");        
+let answer3 = document.querySelector("#answer3");        
+let addimg = document.querySelector("#addimg");        
 
+but2.style.display = "none";
+but2.disabled = true;
 
 let quesv = "";
 let userInput = "";
@@ -17,9 +22,33 @@ function heading(title) {
     });
 }
     
+function fetchImage(title) {
+  const newTitle = encodeURIComponent(title);
+
+
+  fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&titles=${newTitle}&prop=pageimages&piprop=original&format=json&origin=*`
+  )
+    .then(res => res.json())
+    .then(data => {
+      const page = Object.values(data.query.pages)[0];
+
+      if (!page.original) {
+        answer3.innerText = "No image found";
+        return;
+      }
+
+      addimg.src = page.original.source;
+    });
+}
     
+let img = "";
     
 but.addEventListener("click", () => {
+  but.disabled = true;
+  setTimeout(()=>{
+    but.disabled = false;
+  },5000);
     userInput = ques.value.trim();
     if (!userInput) return;
     
@@ -40,7 +69,18 @@ but.addEventListener("click", () => {
 
         const title = results[0].title;
         answer.innerText=title;
+      img = title;
 
         heading(title);
+      but2.style.display = "block";
+but2.disabled = false;
     });
 }); 
+
+but2.addEventListener("click", () => {
+  but.disabled = true;
+  setTimeout(()=>{
+    but.disabled = false;
+  },5000)
+  fetchImage(img); 
+});
